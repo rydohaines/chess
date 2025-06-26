764ee855a19b3a773d0f2b386f665dad1f615751
rydohaines <rydohaines03@gmail.com>
2025-06-26 12:05:45 -0600

    All tests pass

--- shared/src/main/java/chess/PawnMovesCalculator.java
+++ shared/src/main/java/chess/PawnMovesCalculator.java
@@ -17,6 +17,9 @@ public class PawnMovesCalculator implements PieceMovesCalculator{
                     if (board.getPiece(position.positiveDiagonal()).getTeamColor() != board.getPiece(position).getTeamColor()) {
                         if (position.positiveDiagonal().getRow() == 8) {
                             validMoves.add(new ChessMove(position, position.positiveDiagonal(), ChessPiece.PieceType.QUEEN));
+                            validMoves.add(new ChessMove(position, position.positiveDiagonal(), ChessPiece.PieceType.ROOK));
+                            validMoves.add(new ChessMove(position, position.positiveDiagonal(), ChessPiece.PieceType.KNIGHT));
+                            validMoves.add(new ChessMove(position, position.positiveDiagonal(), ChessPiece.PieceType.BISHOP));
                         } else {
                             validMoves.add(new ChessMove(position, position.positiveDiagonal(), null));
                         }
@@ -28,6 +31,9 @@ public class PawnMovesCalculator implements PieceMovesCalculator{
                     if (board.getPiece(position.leftUpDiagonal()).getTeamColor() != board.getPiece(position).getTeamColor()) {
                         if (position.leftUpDiagonal().getRow() == 8) {
                             validMoves.add(new ChessMove(position, position.leftUpDiagonal(), ChessPiece.PieceType.QUEEN));
+                            validMoves.add(new ChessMove(position, position.leftUpDiagonal(), ChessPiece.PieceType.ROOK));
+                            validMoves.add(new ChessMove(position, position.leftUpDiagonal(), ChessPiece.PieceType.KNIGHT));
+                            validMoves.add(new ChessMove(position, position.leftUpDiagonal(), ChessPiece.PieceType.BISHOP));
                         } else {
                             validMoves.add(new ChessMove(position, position.leftUpDiagonal(), null));
                         }
@@ -36,8 +42,15 @@ public class PawnMovesCalculator implements PieceMovesCalculator{
             }
             if(position.upOne() != null) {
                 if (board.getPiece(position.upOne()) == null) {
-                    validMoves.add(new ChessMove(position, position.upOne(), null));
-
+                    if(position.upOne().getRow() == 8 ){
+                        validMoves.add(new ChessMove(position,position.upOne(), ChessPiece.PieceType.QUEEN));
+                        validMoves.add(new ChessMove(position, position.upOne(), ChessPiece.PieceType.ROOK));
+                        validMoves.add(new ChessMove(position, position.upOne(), ChessPiece.PieceType.KNIGHT));
+                        validMoves.add(new ChessMove(position, position.upOne(), ChessPiece.PieceType.BISHOP));
+                    }
+                    else {
+                        validMoves.add(new ChessMove(position, position.upOne(), null));
+                    }
                     if (position.getRow() == 2 && board.getPiece(position.upOne().upOne()) == null && position.upOne().getRow() != position.upOne().upOne().getRow()) {
                         validMoves.add(new ChessMove(position, position.upOne().upOne(), null));
                     }
@@ -49,8 +62,11 @@ public class PawnMovesCalculator implements PieceMovesCalculator{
             if(position.negativeDiagonal() != null) {
                 if (board.getPiece(position.negativeDiagonal()) != null) {
                     if (board.getPiece(position.negativeDiagonal()).getTeamColor() != board.getPiece(position).getTeamColor()) {
-                        if (position.negativeDiagonal().getRow() == 8) {
+                        if (position.negativeDiagonal().getRow() == 1) {
                             validMoves.add(new ChessMove(position, position.negativeDiagonal(), ChessPiece.PieceType.QUEEN));
+                            validMoves.add(new ChessMove(position, position.negativeDiagonal(), ChessPiece.PieceType.ROOK));
+                            validMoves.add(new ChessMove(position, position.negativeDiagonal(), ChessPiece.PieceType.KNIGHT));
+                            validMoves.add(new ChessMove(position, position.negativeDiagonal(), ChessPiece.PieceType.BISHOP));
                         } else {
                             validMoves.add(new ChessMove(position, position.negativeDiagonal(), null));
                         }
@@ -60,8 +76,11 @@ public class PawnMovesCalculator implements PieceMovesCalculator{
             if(position.rightDownDiagonal()!= null) {
                 if (board.getPiece(position.rightDownDiagonal()) != null) {
                     if (board.getPiece(position.rightDownDiagonal()).getTeamColor() != board.getPiece(position).getTeamColor()) {
-                        if (position.rightDownDiagonal().getRow() == 8) {
+                        if (position.rightDownDiagonal().getRow() == 1) {
                             validMoves.add(new ChessMove(position, position.rightDownDiagonal(), ChessPiece.PieceType.QUEEN));
+                            validMoves.add(new ChessMove(position, position.rightDownDiagonal(), ChessPiece.PieceType.ROOK));
+                            validMoves.add(new ChessMove(position, position.rightDownDiagonal(), ChessPiece.PieceType.KNIGHT));
+                            validMoves.add(new ChessMove(position, position.rightDownDiagonal(), ChessPiece.PieceType.BISHOP));
                         } else {
                             validMoves.add(new ChessMove(position, position.rightDownDiagonal(), null));
                         }
@@ -70,7 +89,14 @@ public class PawnMovesCalculator implements PieceMovesCalculator{
             }
             if(position.downOne() != null) {
                 if (board.getPiece(position.downOne()) == null) {
-                    validMoves.add(new ChessMove(position, position.downOne(), null));
+                    if(position.downOne().getRow() == 1 ){
+                        validMoves.add(new ChessMove(position,position.downOne(), ChessPiece.PieceType.QUEEN));
+                        validMoves.add(new ChessMove(position, position.downOne(), ChessPiece.PieceType.ROOK));
+                        validMoves.add(new ChessMove(position, position.downOne(), ChessPiece.PieceType.KNIGHT));
+                        validMoves.add(new ChessMove(position, position.downOne(), ChessPiece.PieceType.BISHOP));
+                    }else {
+                        validMoves.add(new ChessMove(position, position.downOne(), null));
+                    }
                     if (position.getRow() == 7 && board.getPiece(position.downOne().downOne()) == null && position.downOne().getRow() != position.downOne().downOne().getRow()) {
                         validMoves.add(new ChessMove(position, position.downOne().downOne(), null));
                     }

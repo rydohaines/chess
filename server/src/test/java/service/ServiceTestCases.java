d6224457fa526cd91a3c033166ecc57695709c5a
rydohaines <rydohaines03@gmail.com>
2025-07-15 16:09:21 -0600

    added test cases for Register and clear

---
+++ server/src/test/java/service/ServiceTestCases.java
@@ -0,0 +1,50 @@
+package service;
+
+import dataaccess.*;
+import model.UserData;
+import service.ResponsesRequests.RegisterRequest;
+import service.ResponsesRequests.RegisterResponse;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Objects;
+
+
+
+public class ServiceTestCases {
+    @Test
+    public void positiveRegister() throws DataAccessException {
+        UserDAO userDataAccess = new MemoryUserDAO();
+        AuthDAO authDataAccess = new MemoryAuthDAO();
+        UserService userService = new UserService(userDataAccess,authDataAccess);
+        RegisterRequest request = new RegisterRequest("NewUsername","newpassword","newuser@test.com");
+        RegisterResponse result = userService.register(request);
+        assertEquals("NewUsername", result.username());
+        assertNotNull(result.authToken());
+    }
+    @Test
+    public void negativeRegister() throws DataAccessException{
+        UserDAO userDataAccess = new MemoryUserDAO();
+        AuthDAO authDataAccess = new MemoryAuthDAO();
+        UserService userService = new UserService(userDataAccess,authDataAccess);
+        RegisterRequest request = new RegisterRequest("NewUsername","newpassword","newuser@test.com");
+        RegisterResponse result = userService.register(request);
+        RegisterRequest repeatRequest = new RegisterRequest("NewUsername","newpassword","newuser@test.com");
+        assertThrows(DataAccessException.class, () -> userService.register(repeatRequest));
+    }
+    @Test
+    public void positiveClear() throws DataAccessException {
+        UserDAO userDataAccess = new MemoryUserDAO();
+        AuthDAO authDataAccess = new MemoryAuthDAO();
+        GameDAO gameDataAccess = new MemoryGameDAO();
+        ClearService clearService = new ClearService(userDataAccess,authDataAccess,gameDataAccess);
+        userDataAccess.addUser(new UserData("newUser","password","email"));
+        authDataAccess.addAuth("newUser");
+        gameDataAccess.createGame("testGame");
+        clearService.clear();
+
+        assertNull(userDataAccess.getUser("newUser"));
+        assertNull(authDataAccess.getAuth("newUser"));
+        assertTrue(gameDataAccess.listGames().isEmpty());
+    }
+    @Test
+}
